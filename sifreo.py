c1098a3f5e89469b78f6c285cab71b0348a9bc02
AnkaX404 <immortalanka@protonmail.com>
2025-07-16 15:51:58 +0300

    parolao.py

--- sifreo.py
+++ sifreo.py
@@ -5,15 +5,12 @@ from colorama import Fore, Style, init
 init(autoreset=True)
 import os
 
-## Şifreleri İncele ##
 def sifreleri_incele():
 	os.system('clear')
 	with open("sifreler.txt", "r", encoding="utf-8") as dosya:
 		oku = dosya.read()
-		print(Fore.LIGHTGREEN_EX + oku)
-######################
+		print(Fore.LIGHTGREEN_EX + oku
 
-## Şifre Kaydetme Fonksiyonu ##
 def sifre_olustur():
 	isim = input("Şifrenin Hangi Uygulamaya veya Siteye Ait Olacak: ")
 	rast_karakter = string.ascii_letters + string.digits + string.punctuation
@@ -25,9 +22,7 @@ def sifre_olustur():
 	with open("sifreler.txt", "a", encoding="utf-8") as dosya2:
 		dosya2.write(f"\n{isim}: {sifre}\n")
 	print(Fore.LIGHTGREEN_EX + "\nŞifre Dosyaya Yazıldı ✅")
-#########################
 
-## Giriş Menüsü ##
 def ana_menu():
 	while True:
 		os.system('clear')
@@ -44,6 +39,5 @@ def ana_menu():
 			os.system('clear')
 			sifre_olustur()
 		input("\nGeri Dönmek İçin Enter'a Bas.")
-#########################
 
 ana_menu()
